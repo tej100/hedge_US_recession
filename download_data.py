5b77cc50f8d1bc7a931c5fcee002ec3dcfaaa463
tejup <tejuppana@gmail.com>
2023-04-05 19:03:55 -0400

    Create framework for automatic data download

--- download_data.py
+++ download_data.py
@@ -2,8 +2,36 @@
 # it to a csv file to automate the process of downloading data as time goes on
 
 import pandas as pd
-import numpy as np
 import yfinance as yf
+from loguru import logger
 
-alts = yf.download(["SHY", "IEF", "TLT", "BTC-USD", "ETH-USD", "BZ=F", "GC=F", "VNQ"], start="2000-01-01",  end="2023-01-01", group_by="tickers")  # iShares 1-3 | iShares 7-10 | iShares 20+ yrs
-alts = alts.rename(columns={"SHY":"T3","IEF":"T10","TLT":"T20", "BTC-USD":"BTC", "ETH-USD":"ETH", "BZ=F":"OIL", "GC=F":"GOLD", "VNQ":"REST"})
+#############
+# Arguments #
+#############
+
+start = "2000-01-01"
+end = "2023-01-01"
+
+tickers = {
+    # ticker : name
+    "SHY" : "T3",
+    "IEF" : "T10",
+    "TLT" : "T20",
+    "SPY" : "SP500",
+    "QQQ" : "NASDAQ",
+    "DIA" : "DOW",
+    "BTC-USD" : "BTC",
+    "ETH-USD" : "ETH",
+    "BZ=F" : "OIL",
+    "GC=F" : "GOLD",
+    "VNQ" : "REST"
+}
+
+#################
+# Download Data #
+#################
+for k, v in tickers.items():
+    data = pd.DataFrame(yf.download(k, start=start, end=end, progress=False))
+    logger.success(f"{v}: Data Downloaded Successfully")
+    data.to_csv("RAW_DATA/" + v + ".csv", header=True, index_label="Date")
+    logger.success(f"{v}: Data Saved Successfully")
